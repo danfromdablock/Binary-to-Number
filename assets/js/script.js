7f25ab11beb04ab693de728aa86b04beacab5d64
Gustavo Cortez <gustavo.daniel.cortez.gutierrez@student.howest.be>
2024-05-29 23:01:49 +0200

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
 function convertToDecimal() {
     const binaryInput = document.getElementById('binaryInput').value;
-    const resultElement = document.getElementById('result');
+    const decimalResultElement = document.getElementById('decimalResult');
 
     if (binaryInput.match(/^[01]+$/)) {
         const decimalNumber = parseInt(binaryInput, 2);
-        resultElement.innerText = `Decimal: ${decimalNumber}`;
+        decimalResultElement.innerText = `Decimal: ${decimalNumber}`;
     } else {
-        resultElement.innerText = 'Please enter a valid binary number';
+        decimalResultElement.innerText = 'Please enter a valid binary number';
     }
+}
+
+function convertToBinary() {
+    const decimalInput = document.getElementById('decimalInput').value;
+    const binaryResultElement = document.getElementById('binaryResult');
+
+    if (decimalInput.match(/^\d+$/)) {
+        const binaryNumber = parseInt(decimalInput, 10).toString(2).padStart(8, '0');
+        binaryResultElement.innerText = `Binary: ${binaryNumber}`;
+    } else {
+        binaryResultElement.innerText = 'Please enter a valid decimal number';
+    }
+}
+
+function refreshPage() {
+    document.getElementById('binaryInput').value = '';
+    document.getElementById('decimalInput').value = '';
+    document.getElementById('decimalResult').innerText = '';
+    document.getElementById('binaryResult').innerText = '';
 }
